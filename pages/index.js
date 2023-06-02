8d2d0e451b26a07a88097c24f4480d0b9c477a72
Sergio Pereira Sena <senanetworker@gmail.com>
2023-06-02 14:39:47 -0300

    Autenticação de tela de login part 1

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+import { useEffect, useState } from 'react'
 import Login from '../componentes/login'
+import UsuarioService from '@/services/UsuarioService';
+import Home from '@/componentes/home';
+
+const usuarioService = new UsuarioService();
 
 export default function Index() {
-  return (
-    <Login />
-  )
-}
+  const [estaAutenticado, setEstaAutenticado] = useState(false);
 
+  useEffect(() => {
+    setEstaAutenticado(usuarioService.estaAutenticado());
+  }, []);
+
+  if (estaAutenticado) {
+    return <Home />;
+  }
+
+  return <Login />;
+}
--- services/UsuarioService.js
+++ services/UsuarioService.js
@@ -8,11 +8,19 @@ export default class UsuarioService extends httpServices {
         localStorage.setItem("nome", data.nome);
         localStorage.setItem("email", data.email);
         localStorage.setItem("token", data.token)
-        if(data.avatar){
-            localStorage.setItem("avatar", data.avatar);
+
+        const usuario = await this.get('/usuario');
+        localStorage.setItem('id', usuario.data._id)
+
+        if (usuario.data.avatar) {
+            localStorage.setItem("avatar", usuario.data.avatar);
         }
     }
     async cadastro(dados) {
         return this.post('/cadastro', dados);
+    }
+
+    estaAutenticado() {
+        return localStorage.getItem('token') !== null;
     }
 }
