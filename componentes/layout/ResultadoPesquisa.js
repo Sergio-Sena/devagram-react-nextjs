6b744f007d8125edb98f831553641ef0a4845dfc
Sergio Pereira Sena <senanetworker@gmail.com>
2023-06-05 18:46:54 -0300

    Pesquisa e navbar

---
+++ componentes/layout/ResultadoPesquisa.js
@@ -0,0 +1,14 @@
+import Avatar from "../avatar";
+
+export default function ResultadoPesquisa({ nome, email, avatar, onClick, id }) {
+    return (
+        <div className="resultadoPesquisa" onClick={() => onClick(id)}>
+            <Avatar src={avatar} />
+            <div className="informacaoUsuario">
+                <strong>{nome}</strong>
+                <span>{email}</span>
+
+            </div>
+        </div>
+    );
+}
--- componentes/layout/header.js
+++ componentes/layout/header.js
@@ -2,8 +2,48 @@ import Image from "next/image";
 import logoHorizontalImg from "../../public/imagens/logoHorizontal.svg";
 import lupa from "../../public/imagens/lupa.svg";
 import Footer from "./Navegacao";
+import { useState } from "react";
+import ResultadoPesquisa from "./ResultadoPesquisa";
 
 export default function Header() {
+    const [resultadoPesquisa, setResultadoPesquisa] = useState([]);
+    const [termoPesquisado, setTermoPesquisado] = useState([]);
+
+    const aoPesquisar = (e) => {
+        setTermoPesquisado(e.target.value);
+        setResultadoPesquisa([]);
+        
+        if (termoPesquisado < 3) {
+            return;
+        }
+        setResultadoPesquisa([
+            {
+                avatar: '',
+                nome: 'Douglas',
+                email: 'sergio@gmail.com',
+                _id: 123456
+            },
+            {
+                avatar: '',
+                nome: 'Dani',
+                email: 'Dani@gmail.com',
+                _id: 78910
+            },
+            {
+                avatar: '',
+                nome: 'rapahel',
+                email: 'rapahel@gmail.com',
+                _id: 121314
+            }
+        ])
+    }
+
+    const aoClicarResultadoPesquisa = (id) => {
+        console.log('aoClicarResultadoPesquisa', { id })
+
+
+    }
+
     return (
         <header className="cabecalhoHeader">
             <div className="contudoCabecalhoPrincipal">
@@ -25,13 +65,30 @@ export default function Header() {
                     <input
                         type="text"
                         placeholder="Pesquisar"
-                        value={''}
-                        onChange={() => console.log("Pesquisando")}
+                        value={termoPesquisado}
+                        onChange={aoPesquisar}
 
                     />
                 </div>
                 <Footer className="desktop" />
             </div>
+            {resultadoPesquisa.length > 0 && (
+                <div className="resultadoPesquisaContainer">
+                    {
+                        resultadoPesquisa.map(r => (
+                            <ResultadoPesquisa
+                                avatar={r.avatar}
+                                nome={r.nome}
+                                email={r.email}
+                                key={r._id}
+                                id={r._id}
+                                onClick={aoClicarResultadoPesquisa}
+                            />
+                        )
+
+                        )}
+                </div>
+            )}
         </header>
 
     )
