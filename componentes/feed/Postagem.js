7babea1a3643b4cbffa74a8039f885580c61a2be
Sergio Pereira Sena <senanetworker@gmail.com>
2023-07-11 19:57:48 -0300

    correcoes Api

--- componentes/feed/Postagem.js
+++ componentes/feed/Postagem.js
@@ -13,127 +13,116 @@ import imgComentarioAtivo from "../../public/imagens/comentarioAtivo.svg";
 import imgComentarioCinza from "../../public/imagens/comentarioCinza.svg";
 
 const tamanhoLimiteDescricao = 86;
-const feddServce = new FeedService();
-
+const feedService = new FeedService();
 
 export default function Postagem({
-    id,
-    usuario,
-    fotoDoPost,
-    descricao,
-    comentarios,
-    usuarioLogado
+  id,
+  usuario,
+  fotoDoPost,
+  descricao,
+  comentarios,
+  usuarioLogado
 }) {
-    const [deveExibirSecaoComentar, setDeveExibirSecaoComentar] = useState(false);
-    const [tamanhoAtualDescricao, setTamanhoAtualDescricao] = useState(
-        tamanhoLimiteDescricao
-    );
+  const [deveExibirSecaoComentar, setDeveExibirSecaoComentar] = useState(false);
+  const [tamanhoAtualDescricao, setTamanhoAtualDescricao] = useState(tamanhoLimiteDescricao);
 
-    const descricaoCompleta = () => {
-        setTamanhoAtualDescricao(Number.MAX_SAFE_INTEGER);
-    };
+  const descricaoCompleta = () => {
+    setTamanhoAtualDescricao(Number.MAX_SAFE_INTEGER);
+  };
 
-    const descricaoMaiorQueLimite = () => {
-        return descricao.length > tamanhoAtualDescricao;
-    };
+  const descricaoMaiorQueLimite = () => {
+    return descricao.length > tamanhoAtualDescricao;
+  };
 
-    const obterDescricao = () => {
-        let mensagem = descricao.substring(0, tamanhoAtualDescricao);
-        if (descricaoMaiorQueLimite()) {
-            mensagem += "...";
-        }
-        return mensagem;
-    };
-
-    const obterImagemComentario = () => {
-        return deveExibirSecaoComentar
-            ? imgComentarioAtivo
-            : imgComentarioCinza;
+  const obterDescricao = () => {
+    let mensagem = descricao.substring(0, tamanhoAtualDescricao);
+    if (descricaoMaiorQueLimite()) {
+      mensagem += "...";
     }
+    return mensagem;
+  };
 
-    const comentar = async (comentario) => {
-        try {
-            await feedService.adicionarComentario(id, comentario);
-            setDeveExibirSecaoParaComentar(false);
-            setComentariosPostagem([
-                ...comentariosPostagem,
-                {
-                    nome: usuarioLogado.nome,
-                    mensagem: comentario
-                    
-                }
-            ]);
-            return true;
-        } catch (e) {
-            alert(`Erro ao fazer comentario! ` + (e?.response?.data?.erro || ''));
-        }
+  const obterImagemComentario = () => {
+    return deveExibirSecaoComentar ? imgComentarioAtivo : imgComentarioCinza;
+  };
+
+  const comentar = async (comentario) => {
+    try {
+      await feedService.adicionarComentario(id, comentario);
+      setDeveExibirSecaoComentar(false);
+      setComentarios([...comentarios, { nome: usuarioLogado.nome, mensagem: comentario }]);
+      return true;
+    } catch (e) {
+      alert(`Erro ao fazer comentário! ` + (e?.response?.data?.erro || ''));
     }
+  };
+  
 
-    return (
-        <div className="postagem">
-            {usuario && usuario.id && (
-                <Link href={`/perfil/${usuario.id}`}>
-                    <section className="headerPostagem">
-                        <Avatar src={usuario.avatar} />
-                        <strong>{usuario.nome}</strong>
-                    </section>
-                </Link>
+  return (
+    <div className="postagem">
+      {usuario && usuario.id && (
+        <Link href={`/perfil/${usuario.id}`}>
+          <section className="headerPostagem">
+            <Avatar src={usuario.avatar} />
+            <strong>{usuario.nome}</strong>
+          </section>
+        </Link>
+      )}
+      <div className="fotoDaPostagem">
+        <img src={fotoDoPost} alt="Foto da postagem" />
+      </div>
+      <div className="rodapeDaPostagem">
+        <div className="acoesDaPostagem">
+          <Image
+            src={imgCurtir}
+            alt="icone Curtir"
+            width={20}
+            height={20}
+            onClick={() => console.log("curtir")}
+          />
+          <Image
+            src={obterImagemComentario()}
+            alt="icone comentar"
+            width={20}
+            height={20}
+            onClick={() =>
+              setDeveExibirSecaoComentar(!deveExibirSecaoComentar)
+            }
+          />
+          <span className="quantidadedeCurtidas">
+            Curtido por <strong>50 pessoas</strong>
+          </span>
+          <div className="descricaoDaPostagem">
+            {usuario && usuario.nome && (
+              <strong className="nomeUsuario">{usuario.nome}</strong>
             )}
-            <div className="fotoDaPostagem">
-                <img src={fotoDoPost} alt="Foto da postagem" />
-            </div>
-            <div className="rodapeDaPostagem">
-                <div className="acoesDaPostagem">
-                    <Image
-                        src={imgCurtir}
-                        alt="icone Curtir"
-                        width={20}
-                        height={20}
-                        onClick={() => console.log("curtir")}
-                    />
-                    <Image
-                        src={obterImagemComentario()}
-                        alt="icone comentar"
-                        width={20}
-                        height={20}
-                        onClick={() =>
-                            setDeveExibirSecaoComentar(!deveExibirSecaoComentar)
-                        }
-                    />
-                    <span className="quantidadedeCurtidas">
-                        Curtido por <strong>50 pessoas</strong>
-                    </span>
-                    <div className="descricaoDaPostagem">
-                        {usuario && usuario.nome && (
-                            <strong className="nomeUsuario">{usuario.nome}</strong>
-                        )}
-                        <p className="descricao">
-                            {obterDescricao()}
-                            {descricaoMaiorQueLimite() && (
-                                <span onClick={descricaoCompleta} className="descricaoCompleta">
-                                    mais
-                                </span>
-                            )}
-                        </p>
-                    </div>
-                </div>
-                <div className="comentariosDaPublicacao">
-                    {comentarios.map((comentario, i) => (
-                        <div className="comentario" key={i}>
-                            {comentario.nome && (
-                                <strong className="nomeUsuario">{comentario.nome}</strong>
-                            )}
-                            <p className="descricao">{comentario.mensagem}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-            {deveExibirSecaoComentar && (
-                <div>
-                    <FazerComentario comentar={comentar} usuarioLogado={usuarioLogado} />
-                    <Feed usuarioLogado={usuarioLogado} /> {/* Renderize o componente Feed */}
-                </div>
-            )}
+            <p className="descricao">
+              {obterDescricao()}
+              {descricaoMaiorQueLimite() && (
+                <span onClick={descricaoCompleta} className="descricaoCompleta">
+                  mais
+                </span>
+              )}
+            </p>
+          </div>
         </div>
-    );
+        <div className="comentariosDaPublicacao">
+          {comentarios.map((comentario, i) => (
+            <div className="comentario" key={i}>
+              {comentario.nome && (
+                <strong className="nomeUsuario">{comentario.nome}</strong>
+              )}
+              <p className="descricao">{comentario.mensagem}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {deveExibirSecaoComentar && (
+        <div>
+          <FazerComentario comentar={comentar} usuarioLogado={usuarioLogado} />
+          <Feed usuarioLogado={usuarioLogado} />
+        </div>
+      )}
+    </div>
+  );
 }
